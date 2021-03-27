ee563c65b1d598e54ae5fb896dd632ea775f2f82
CesarMtzV <mc.cesar.art@gmail.com>
2021-03-27 16:05:41 -0500

    Initial Commit

---
+++ Hajime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hajime
+//
+//  Created by Cesar Martinez Valenzuela on 27/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
