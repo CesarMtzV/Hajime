cda58607b28cf1bc3644b8f0c2869bb8510e55e3
Alex Vargas <alexjesusvargas@gmail.com>
2021-04-17 20:23:07 -0500

    started API call implementation

---
+++ Hajime/KanjiStruct.swift
@@ -0,0 +1,62 @@
+//
+//  KanjiStruct.swift
+//  Hajime
+//
+//  Created by Alex Vargas  on 17/04/21.
+//
+
+import Foundation
+
+struct kanjiData: Codable {
+    let kanji:String
+    let grade:Int
+    let stroke:Int
+    let meanings:[String]
+    let kunReading:[String]
+    let onReading:[String]
+    let nameReading:[String]
+    let jlpt:Int
+    let unicode:String
+    let heisig_en:String
+
+}
+
+/*
+ {
+   "kanji": "日",
+   "grade": 1,
+   "stroke_count": 4,
+   "meanings": [
+     "day",
+     "sun",
+     "Japan",
+     "counter for days"
+   ],
+   "kun_readings": [
+     "ひ",
+     "-び",
+     "-か"
+   ],
+   "on_readings": [
+     "ニチ",
+     "ジツ"
+   ],
+   "name_readings": [
+     "あ",
+     "あき",
+     "いる",
+     "く",
+     "くさ",
+     "こう",
+     "す",
+     "たち",
+     "に",
+     "にっ",
+     "につ",
+     "へ"
+   ],
+   "jlpt": 4,
+   "unicode": "65e5",
+   "heisig_en": "day"
+ }
+ */
--- Hajime/ShowMoreViewController.swift
+++ Hajime/ShowMoreViewController.swift
@@ -11,11 +11,34 @@ class ShowMoreViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchKanjiData { (kanji) in
+            for kanjiData in kanji {
+                print(kanjiData.kanji)
 
-        // Do any additional setup after loading the view.
+            }
+        }
+
+        
     }
     
-
+    func fetchKanjiData(completionHandler: @escaping ([kanjiData]) -> Void){
+        let url = URL(string: "https://kanjiapi.dev/v1/kanji/%E6%97%A5")!
+        
+        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+            
+            guard let data=data else {return}
+            
+            do{
+                let KanjiData = try JSONDecoder().decode([kanjiData].self, from: data)
+                
+                completionHandler(KanjiData)
+            }catch{ 
+                let error = error
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
+            
     /*
     // MARK: - Navigation
 
@@ -24,6 +47,32 @@ class ShowMoreViewController: UIViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
+     
+     private func getKanji(from url: String){
+         let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: {data, response, error in
+             
+             guard let data=data, error == nil else {
+                 print("Something went wrong")
+                 return
+             }
+             
+             var result: kanjiData?
+             do{
+                 result = try JSONDecoder().decode(kanjiData.self, from: data)
+             }catch{
+                 print("failed")
+             }
+             
+             guard let json = result else{
+                 return
+             }
+             
+             print(json.kanji)
+         })
+         
+         task.resume()
+     }
     */
 
 }
+
