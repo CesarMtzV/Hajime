c1fca226689b8d52395848d538495cac67c98d55
CesarMtzV <mc.cesar.art@gmail.com>
2021-04-21 11:39:57 -0500

    Added basic navigation to Katakana section

--- Hajime/ViewControllers/Katakana/KatakanaViewController.swift
+++ Hajime/ViewControllers/Katakana/KatakanaViewController.swift
@@ -8,11 +8,32 @@
 import UIKit
 
 class KatakanaViewController: UIViewController {
+    
+    @IBOutlet weak var btnEstudiar: UIButton!
+    @IBOutlet weak var btnPracticar: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        configurarBotones()
+        configurarNavbar()
+    }
+    
+    func configurarBotones(){
+        btnEstudiar.layer.cornerRadius = 0.5 * btnEstudiar.bounds.size.width
+        btnEstudiar.clipsToBounds = true
+        
+        btnPracticar.layer.cornerRadius = 0.5 * btnPracticar.bounds.size.width
+        btnPracticar.clipsToBounds = true
+    }
+    
+    func configurarNavbar(){
+        
+        let settingsButton = UIBarButtonItem(image: UIImage(systemName: "gear"), style: .done, target: self, action: nil)
+        
+        title = "Katakana"
+        navigationItem.setRightBarButton(settingsButton, animated: true)
     }
     
 
