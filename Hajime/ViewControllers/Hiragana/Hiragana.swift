f78292d8e0099b9cec57c524cb11427ed6a0f88d
CesarMtzV <mc.cesar.art@gmail.com>
2021-04-21 23:52:54 -0500

    Added hiragana grid

---
+++ Hajime/ViewControllers/Hiragana/Hiragana.swift
@@ -0,0 +1,57 @@
+//
+//  Hiragana.swift
+//  Hajime
+//
+//  Created by Cesar Martinez Valenzuela on 21/04/21.
+//
+
+import UIKit
+
+let hiragana: [Kana] = [
+    Kana(symbol: "あ", reading: "a"),
+    Kana(symbol: "い", reading: "i"),
+    Kana(symbol: "う", reading: "u"),
+    Kana(symbol: "え", reading: "e"),
+    Kana(symbol: "お", reading: "o"),
+    Kana(symbol: "か", reading: "ka"),
+    Kana(symbol: "き", reading: "ki"),
+    Kana(symbol: "く", reading: "ku"),
+    Kana(symbol: "け", reading: "ke"),
+    Kana(symbol: "こ", reading: "ko"),
+    Kana(symbol: "さ", reading: "sa"),
+    Kana(symbol: "し", reading: "shi"),
+    Kana(symbol: "す", reading: "su"),
+    Kana(symbol: "せ", reading: "se"),
+    Kana(symbol: "そ", reading: "so"),
+    Kana(symbol: "た", reading: "ta"),
+    Kana(symbol: "ち", reading: "chi"),
+    Kana(symbol: "つ", reading: "tsu"),
+    Kana(symbol: "て", reading: "te"),
+    Kana(symbol: "と", reading: "to"),
+    Kana(symbol: "な", reading: "na"),
+    Kana(symbol: "に", reading: "ni"),
+    Kana(symbol: "ぬ", reading: "nu"),
+    Kana(symbol: "ね", reading: "ne"),
+    Kana(symbol: "の", reading: "no"),
+    Kana(symbol: "は", reading: "ha"),
+    Kana(symbol: "ひ", reading: "hi"),
+    Kana(symbol: "ふ", reading: "fu"),
+    Kana(symbol: "へ", reading: "he"),
+    Kana(symbol: "ほ", reading: "ho"),
+    Kana(symbol: "ま", reading: "ma"),
+    Kana(symbol: "み", reading: "mi"),
+    Kana(symbol: "む", reading: "mu"),
+    Kana(symbol: "め", reading: "me"),
+    Kana(symbol: "も", reading: "mo"),
+    Kana(symbol: "ら", reading: "ra"),
+    Kana(symbol: "り", reading: "ri"),
+    Kana(symbol: "る", reading: "ru"),
+    Kana(symbol: "れ", reading: "re"),
+    Kana(symbol: "ろ", reading: "ro"),
+    Kana(symbol: "や", reading: "ya"),
+    Kana(symbol: "ゆ", reading: "yu"),
+    Kana(symbol: "よ", reading: "yo"),
+    Kana(symbol: "わ", reading: "wa"),
+    Kana(symbol: "を", reading: "wo"),
+    Kana(symbol: "ん", reading: "n"),
+]
