035d7edda81c395d37eae4d03e8fe06d2feffe16
CesarMtzV <mc.cesar.art@gmail.com>
2021-04-24 11:26:44 -0500

    Added basic kanji screen

---
+++ Hajime/ViewControllers/Kanji/KanjiViewController.swift
@@ -0,0 +1,19 @@
+//
+//  KanjiViewController.swift
+//  Hajime
+//
+//  Created by Cesar Martinez Valenzuela on 24/04/21.
+//
+
+import UIKit
+
+class KanjiViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Kanji"
+    }
+    
+
+}
