e71da24c3ea5c36cb2437ea707406f3c27776e2d
CesarMtzV <mc.cesar.art@gmail.com>
2021-05-23 10:09:21 -0500

    Scrollview en el form para añadir un nuevo Kanji funcionando

---
+++ Hajime/ViewControllers/Kanji/AddKanjiViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddKanjiViewController.swift
+//  Hajime
+//
+//  Created by Cesar Martinez Valenzuela on 23/05/21.
+//
+
+import UIKit
+
+class AddKanjiViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
